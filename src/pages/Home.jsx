46c6467a62e3ddfffebd56edd6bb946f291e009d
PRANAV SHILU <pranavshilu1811@gmail.com>
2026-02-18 19:36:49 +0530

    added developed pages to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,27 @@ export default function Home() {
   return (
     <section className="page">
       <h1>Home Page</h1>
+      <h6 style={{ margin: "30px 0 20px" }}>Curently developed pages:</h6>
+
       <NavLink
         to="/news"
-        style={{ color: "blue", textDecoration: "underline", fontSize: "40px" }}
+        style={{ color: "blue", textDecoration: "underline", fontSize: "25px" }}
       >
-        Go to News & Blog page
+        News & Blog
+      </NavLink>
+
+      <NavLink
+        to="/privacy-policy"
+        style={{ color: "blue", textDecoration: "underline", fontSize: "25px" }}
+      >
+        Privacy Policy
+      </NavLink>
+
+      <NavLink
+        to="/faqs"
+        style={{ color: "blue", textDecoration: "underline", fontSize: "25px" }}
+      >
+        FAQs
       </NavLink>
     </section>
   );
